e5ac043364627349ec97d40eca76e69f5285bdcf
Suyash <105059093+Suyash-4@users.noreply.github.com>
2023-10-23 19:47:59 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const nameElement = document.querySelector(".name");
-
-    nameElement.addEventListener("animationiteration", function () {
-        nameElement.style.animation = "none"; // Remove the animation
-    });
-});
-
-
